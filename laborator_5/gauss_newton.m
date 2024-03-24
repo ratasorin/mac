89534e9c53b6f16a16905178db004fdef8270085
ratasorin <ratasorin0@gmail.com>
2024-03-24 13:07:58 +0200

    [Fix] Gauss Newton method was not using the initial approximation

--- laborator_5/gauss_newton.m
+++ laborator_5/gauss_newton.m
@@ -1,7 +1,8 @@
 function best_approximation = gauss_newton (residuals, residuals_jacobian, steps, initial_approximation) 
     x = zeros(length(initial_approximation), steps);
-    for i = 1 : steps - 1 
-        x(:, i + 1) = x(:, i) - (transpose(residuals_jacobian(x(:, i))) * residuals_jacobian(x(:, i))) \ (transpose(residuals_jacobian(x(:, i))) * residuals(x(:, i)));
+    x(:, 1) = initial_approximation;
+    for i = 1 : steps - 1
+        x(:, i + 1) = x(:, i) - (transpose(residuals_jacobian(x(:, i))) * residuals_jacobian(x(:, i))) \ transpose(residuals_jacobian(x(:, i))) * residuals(x(:, i));
     end
     best_approximation = x(:, steps);
 end
